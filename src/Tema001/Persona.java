763ba81791b0a716d259043e2d2b8244ce6d7b56
manuel@AIO.universidad <manuel@masiasweb.com>
2024-09-16 11:50:40 +0200

    Ajuste

---
+++ src/Tema001/Persona.java
@@ -0,0 +1,15 @@
+class Persona {
+    private String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
--- src/Tema001/Tema001.java
+++ src/Tema001/Tema001.java
@@ -97,24 +97,4 @@ class Tema001 {
         return System.identityHashCode(unaCadena);
     }
  
-    
-
-    
 }
-
-class Persona {
-    private String nombre;
-
-    public Persona(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-}
-
