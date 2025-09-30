6237c6366fb3e90ce8d99f0da04fdfd390de6310
manuel@oficina <manuel@masiasweb.com>
2025-09-30 11:11:06 +0200

    feat: agregar clase Wtf con ejemplos de comparación de objetos Integer

---
+++ src/zzz-otros/Wtf.java
@@ -0,0 +1,10 @@
+public class Wtf {
+    public static void main(String[] args) {
+        Integer a = 10;
+        Integer b = 10;
+        Integer c = 1000;
+        Integer d = 1000;
+        System.out.println(a == b);
+        System.out.println(c == d);
+    }
+}
